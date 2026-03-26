0c14a058bdc2757a44a0bf665e2077b9fe414740
meenakshij555 <meenakshi.j555@gmail.com>
2026-03-26 18:38:28 +0530

    New project

---
+++ src/packOne/one.java
@@ -0,0 +1,10 @@
+package packOne;
+
+public class one {
+    static int b;
+    public static void main(String[] args) {
+        int a=3;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
